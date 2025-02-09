d7659e859bbb34f86e20b59c624fa89d055f9b7d
Retskia <reetta.karki00@gmail.com>
2025-02-09 19:39:24 +0200

    ApplooksEnchancedandNotificationsadded

--- Memorando/Assets/Scripts/MoveScene.cs
+++ Memorando/Assets/Scripts/MoveScene.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MoveScene : MonoBehaviour
 {
+    public TextMeshProUGUI DebugLabelA;
 
     void Start()
     {
+
         StartCoroutine(MoveToNext());
+        DebugLabelA.SetText(PlayerPrefs.GetInt("HasCompletedFirstLaunch", 0).ToString());
     }
 
     IEnumerator MoveToNext()
     {
         yield return new WaitForSeconds(2);
 
-        if (PlayerPrefs.GetInt("HasCompletedLaunch", 0) == 1)
+        if (PlayerPrefs.GetInt("HasCompletedFirstLaunch", 0) == 1)
         {
             SceneManager.LoadScene("HomeScene");
         }
